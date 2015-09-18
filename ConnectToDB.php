ec9b08922b090eb30b2ba40c2c17b1bc0c11f3f0
udafend <bucfernando@gmail.com>
2015-09-19 03:04:58 +0530

    changed

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -1,6 +1,6 @@
 <?php
-$con = mysqli_connect("localhost","cropadvisor","","cropadvisor");
-
+$con = mysqli_connect("localhost","cropadvisor","cropadvisor","");
+mysql_select_db('cropadvisor',$con);
 if (mysqli_connect_errno()) 
 {
 	echo "Failed to connect to MySQL: ". mysqli_connect_error();	
--- GetAllProvince.php
+++ GetAllProvince.php
@@ -1,9 +1,6 @@
 <?php
 include("ConnectToDb.php");
 
- mysql_select_db("cropadvisor");
-   
-
  $result= mysqli_query($con,"SELECT * FROM province ");
 /* create one master array of the records */
 	$posts = array();
