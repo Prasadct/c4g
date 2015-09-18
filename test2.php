e671229ea6581d5148ead56426a57dcb4ffc10ea
unknown <UJAYATH@LKSRIWL-9RKLR32.PEROOT.com>
2015-09-19 03:22:23 +0530

    test

--- test2.php
+++ test2.php
@@ -1,4 +1,19 @@
-<?php 
+<?php
+include("ConnectToDb.php");
 
-echo "Helllo";
+$result= mysqli_query($con,"SELECT * FROM province");
+
+$posts = array();
+if(mysql_num_rows($result)) {
+    while($row = mysql_fetch_assoc($result)) {
+        $posts[] = array('post'=>$row);
+    }
+}
+
+
+echo json_encode(array('posts'=>$posts));
+
+
+mysql_close();
+
 ?>
