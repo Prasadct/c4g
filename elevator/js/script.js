4c360c69ea25df8cee0f3d55a0cc85774c28ac5a
dilrukjayawardena <dushancst@gmail.com>
2015-09-19 10:40:43 +0530

    test1

---
+++ elevator/js/script.js
@@ -0,0 +1,68 @@
+var $ = jQuery.noConflict();
+
+
+
+// Progress Bar
+
+$(document).ready(function ($) {
+    "use strict";
+    
+    $('.skill-shortcode').appear(function () {
+        $('.progress').each(function () {
+            $('.progress-bar').css('width',  function () { return ($(this).attr('data-percentage') + '%')});
+        });
+    }, {accY: -100});
+        
+        
+});
+
+function addTable(str) {	
+     var xmlhttp;    
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+	var myTableDiv = document.getElementById("DynamicTable");
+      
+    var table = document.createElement('TABLE');
+	table.setAttribute('id','dist');
+    table.border='1';
+    
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+      
+    for (var i=0; i<3; i++){
+       var tr = document.createElement('TR');
+       tableBody.appendChild(tr);
+       
+       for (var j=0; j<4; j++){
+           var td = document.createElement('TD');
+           td.width='75';
+           td.appendChild(document.createTextNode("Cell " + i + "," + j));
+           tr.appendChild(td);
+       }
+    }
+    myTableDiv.appendChild(table);
+    }
+  }
+xmlhttp.open("GET","http://localhost:8080/Project/GetDetailsForMainType.php?mainType=Disease&cropId="+str ,true);
+xmlhttp.send(); 
+    
+    
+}
+ function DeleteRows() {
+	 
+	 var table=document.getElementById('dist')
+        var rowCount = table.rows.length;
+        for (var i = rowCount-1; i >=0; i--) {
+            table.deleteRow(i);
+ }}
