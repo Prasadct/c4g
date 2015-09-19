7cb9dbea682d40a57c83b4bebec045a3c7e623eb
unknown <UJAYATH@LKSRIWL-9RKLR32.PEROOT.com>
2015-09-20 00:18:27 +0530

    change js

--- elevator/js/script.js
+++ elevator/js/script.js
@@ -62,7 +62,7 @@ function addTableReportDisease(str) {
         success: function(data) {
             for(var i = 0; i < data.length ; i++){
                 console.log(JSON.stringify(data[i]));
-                $('#row_entry').after('<tr class="report"><td>'+data['title']+'</td><th>'+data['details']+'</th><th><img src="http://128.199.125.48/photos/'+data['image']+'"/></th><th>Status</th></tr>');
+                $('#row_entry').after('<tr class="report"><td>'+data[i]['title']+'</td><th>'+data[i]['details']+'</th><th><img src="http://128.199.125.48/photos/'+data[i]['image']+'"/></th><th>'+data[i]['is_solved']+'</th></tr>');
             }
         },
         data: {},
