d674daf77af9ef7056bb5d0c9d181ed17fd82846
dilrukjayawardena <dushancst@gmail.com>
2015-09-19 11:35:40 +0530

    test1.2.2

--- elevator/js/script.js
+++ elevator/js/script.js
@@ -31,27 +31,7 @@ xmlhttp.onreadystatechange=function()
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
     document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
-	var myTableDiv = document.getElementById("DynamicTable");
-      
-    var table = document.createElement('TABLE');
-	table.setAttribute('id','dist');
-    table.border='1';
-    
-    var tableBody = document.createElement('TBODY');
-    table.appendChild(tableBody);
-      
-    for (var i=0; i<3; i++){
-       var tr = document.createElement('TR');
-       tableBody.appendChild(tr);
-       
-       for (var j=0; j<4; j++){
-           var td = document.createElement('TD');
-           td.width='75';
-           td.appendChild(document.createTextNode("Cell " + i + "," + j));
-           tr.appendChild(td);
-       }
-    }
-    myTableDiv.appendChild(table);
+	
     }
   }
 xmlhttp.open("GET","http://128.199.125.48/GetDetailsForMainType.php?mainType=ReportedDisease&cropId="+str ,true);
@@ -61,8 +41,5 @@ xmlhttp.send();
 }
  function DeleteRows() {
 	 
-	 var table=document.getElementById('dist')
-        var rowCount = table.rows.length;
-        for (var i = rowCount-1; i >=0; i--) {
-            table.deleteRow(i);
+	
  }}
