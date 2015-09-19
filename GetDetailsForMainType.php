e9a275b4dfe3a64e333f290f3292fb44f9ab2c30
unknown <UJAYATH@LKSRIWL-9RKLR32.PEROOT.com>
2015-09-19 11:50:52 +0530

    fix GetDetailsForMainType

--- GetDetailsForMainType.php
+++ GetDetailsForMainType.php
@@ -5,6 +5,7 @@ include( dirname(__FILE__)."/ConnectToDB.php");
 $mainType = $_GET['mainType'] ;
 $cropId= $_GET['cropId'] ;
 
+
 	/* grab the posts from the db */
 	if ($mainType == "Disease")
 	{
@@ -12,28 +13,28 @@ $cropId= $_GET['cropId'] ;
 	}
 	elseif($mainType == "Detail")
 	{
-		 $result= mysqli_query($con,"SELECT * FROM detail where crop_id = '$cropId'");
+		 $result= mysqli_query($con,"SELECT * FROM detail where crop_id = '$cropId' ");
 	}
 	elseif($mainType == "ReportDisease")
 	{
-		$result= mysqli_query($con,"SELECT * FROM reprotdesease where crop_id = '$cropId'");
+		$result= mysqli_query($con,"SELECT * FROM reportdesease where crop_id = '$cropId' ");
 	}
 	elseif($mainType == "Support")
 	{
-		$result= mysqli_query($con,"SELECT * FROM support where crop_id = '$cropId'");
+		$result= mysqli_query($con,"SELECT * FROM support where crop_id = '$cropId' ");
 	}
 	
-	
+
 
 	/* create one master array of the records */
 	$posts = array();
-	if(isset($result	) && mysqli_num_rows($result)) {
+	if(isset($result) && mysqli_num_rows($result)) {
 		while($post = mysqli_fetch_array($result)) {
 			$posts[] = $post;
 		}
 	}	
 		echo json_encode($posts);
-		
+			
 mysqli_close($con);
 
 ?>
